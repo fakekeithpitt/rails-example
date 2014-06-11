a05be9435806057eed895986e459962e61d25108
Keith Pitt <me@keithpitt.com>
2014-06-11 21:32:14 +0930

    Bundle update.

--- spec/support/sphinx.rb
+++ spec/support/sphinx.rb
@@ -21,7 +21,7 @@ RSpec.configure do |config|
     ThinkingSphinx::Test.start_with_autostop
   end
 
-  config.before(:each) do
+  config.before(:each) do |example|
     # Index data when running an acceptance spec.
     index if example.metadata[:js]
   end
